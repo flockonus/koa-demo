31b5749156d0ca379979fd0c0a2e65750e34ebcd
FabianoPS <fabianosoriani@gmail.com>
2014-05-29 21:50:42 -0700

    adding middlewares

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ var koa = require('koa');
 var app = koa();
 var PORT = 3000
 
-app.use(function *(){
-  this.body = 'Hello World --'+app.env;
+app.use(function *(next){
+  console.log("\n\n\n\n\n\n")
+  console.log("1A")
+  yield next
+  console.log("1B")
+});
+
+app.use(function *(next){
+  console.log("2A")
+  yield next
+  console.log("2B")
+});
+
+app.use(function *(next){
+  console.log("3A")
+  yield next
+  console.log("3B")
+});
+
+app.use(function *(next){
+  console.log("4A")
+  this.body = "--"
+  yield next
+  console.log("4B")
 });
 
 app.listen(PORT);
