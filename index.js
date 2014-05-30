deca463504e937f1e9b367202167ea6337c0c305
FabianoPS <fabianosoriani@gmail.com>
2014-05-30 11:42:14 -0700

    fetching some pages in parallel

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
 var koa = require('koa');
 var app = koa();
 var PORT = 3000
+var request = require('co-request')
 
-var KoaRender = require('koa-render')
-app.use(KoaRender('./views', 'jade'))
-
-app.use(function *(next){
-  console.log("\n\n\n\n\n\n")
-  console.log("1A")
-  yield next
-  console.log("1B")
+app.use(function *(){
+  var pages = {
+    google: request({uri:"http://google.com"}),
+    axiomZen: request({uri:"http://axiomzen.co"})
+  }
+  this.body = yield pages
 });
 
-app.use(function *(next){
-  console.log("2A")
-  yield next
-  console.log("2B")
-});
-
-app.use(function *(next){
-  console.log("3A")
-  yield next
-  console.log("3B")
-});
-
-app.use(function *(next){
-  console.log("4A")
-  // jade/html
-  this.body = yield this.render('index');
-  yield next
-  console.log("4B")
-});
 
 app.listen(PORT);
 console.log("listening at",PORT)
